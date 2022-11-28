93b554fb38b1ec980acbe918d1047c248a653ab2
amorar05 <amorar05@estudiantes.unileon.es>
2022-11-28 14:33:03 +0100

    Test inicio

--- test/es/unileon/prg1/wordle/WordleTest.java
+++ test/es/unileon/prg1/wordle/WordleTest.java
@@ -1,4 +1,12 @@
 package es.unileon.prg1.wordle;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class WordleTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
 }
