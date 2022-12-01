13f618f9d766c2c3a20b9bd2824191d59b70f088
rdecea00 <rdecea00@estudiantes.unileon.es>
2022-12-01 11:37:20 +0100

    Boarding iniciado

--- src/es/unileon/prg1/wordle/Board.java
+++ src/es/unileon/prg1/wordle/Board.java
@@ -1,5 +1,70 @@
 package es.unileon.prg1.wordle;
+public class Board 
+{
+    private String  word;
+    private int cont;
+    public Board (String word)
+        {
+            this.word = word;
+            cont=0;
+        }
+   public void Comparar(String word)
+    {
+     
+     for(int i=0;i<word.longitud();i++)
+     { 
+      if (this.word[i].equals(word[i]))
+          { 
+           System.out.println("Encontrado"); 
+           cont++;
+          }
+      else 
+          {
+           System.out.println("No encontrado");
+          }
+     }
+         
 
-public class Board {
+    }
+     public int getAciertos{
+         cont=0;
+        }
     
+     public boolean Fin()
+     cont=0;
+     if (cont == word.longitud){
+     return true;
+
+     }
+    public boolean Comparar(dictionary=diccionario)
+    {
+        Dictionary = diccionario;
+        for(int i=0;i<diccionario.size();i++){
+            if (diccionario.get(i)==word){
+                return true;    
+            }
+            else{
+                return false;
+            }
+
+    }
+    public boolean emptyDictionary(){
+      
+        if(diccionario.size == 0){ 
+            return true;
+        }
+        else{
+            return false;
+        }
+      }
+    public int leftTries(){
+        int intentosPosibles=6;
+        int intentosUsados=0;
+        intentos_restantes = intentosPosibles-intentosUsados;
+        return intentos_restantes;
+    }    
+    
+
 }
+
+
